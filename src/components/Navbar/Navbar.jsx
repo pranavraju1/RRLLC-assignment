740f8c6eced131f6a4cf451390422013ee92c65f
Pranav Raju <pranavraju2020@gmail.com>
2024-02-29 14:20:31 +0530

    check

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,3 @@
-// import "./Navbar.scss";
-// const NavigationBar = () => {
-//   return (
-//     // <div className="navigation">
-//     //   <div>
-//     //     <input type="text" />
-//     //   </div>
-//     //   <div className="tags">Categories</div>
-//     //   <div className="tags">Website Builders</div>
-//     //   <div className="tags">Today's deals</div>
-//     // </div>
-//   );
-// };
-
-// export default NavigationBar;
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
